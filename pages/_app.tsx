72e42c440cba746bc5ff62366e7a778bc0005a24
Chris Beresford <chrisberesford85@gmail.com>
2023-05-04 10:04:00 -0400

    fix: add SSRProvider for id generation issues between client and server

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import '@/styles/globals.css';
 import { Inter } from 'next/font/google';
 import Head from 'next/head';
 import NextProgress from 'nextjs-progressbar';
-import { Container } from 'react-bootstrap';
+import { Container, SSRProvider } from 'react-bootstrap';
 
 import NavBar from '@/components/NavBar';
 import styles from '@/styles/App.module.css';
@@ -14,27 +14,29 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<div className={`${inter.className}`}>
-			<Head>
-				<title key='title'>News App</title>
-				<meta
-					name='description'
-					content='News course to practice coding'
-				/>
-				<meta
-					name='viewport'
-					content='width=device-width, initial-scale=1'
-				/>
-				<link
-					rel='icon'
-					href='/favicon.ico'
-				/>
-			</Head>
-			<NextProgress />
-			<NavBar />
-			<Container className={styles.pageContainer}>
-				<Component {...pageProps} />
-			</Container>
-		</div>
+		<SSRProvider>
+			<div className={`${inter.className}`}>
+				<Head>
+					<title key='title'>News App</title>
+					<meta
+						name='description'
+						content='News course to practice coding'
+					/>
+					<meta
+						name='viewport'
+						content='width=device-width, initial-scale=1'
+					/>
+					<link
+						rel='icon'
+						href='/favicon.ico'
+					/>
+				</Head>
+				<NextProgress />
+				<NavBar />
+				<Container className={styles.pageContainer}>
+					<Component {...pageProps} />
+				</Container>
+			</div>
+		</SSRProvider>
 	);
 }
